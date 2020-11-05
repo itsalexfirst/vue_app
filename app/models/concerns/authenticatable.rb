3bd8795ee6af1cc619242b8d621608bc167fc1db
itsalexfirst <itsalex@yandex.ru>
2020-11-05 17:16:33 +0300

    authenticatable concern

---
+++ app/models/concerns/authenticatable.rb
@@ -0,0 +1,10 @@
+module Authenticatable
+  extend ActiveSupport::Concern
+
+  included do
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
+  end
+end
