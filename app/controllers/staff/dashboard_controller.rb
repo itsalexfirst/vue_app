d120e981a5a1720f4a4d5916f53501f459db12a1
itsalexfirst <itsalex@yandex.ru>
2020-11-21 01:03:14 +0300

    TODO memo

--- app/controllers/staff/dashboard_controller.rb
+++ app/controllers/staff/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Staff::DashboardController < ApplicationController
   def index
     render json: {
       current_user: current_staff,
+      #TODO query builder for clients and organization
       clients: Client.all,
       organizations: Organization.all
     }
